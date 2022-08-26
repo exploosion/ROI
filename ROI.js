e49edf5fb0041128e03c8668e870a96a035ec092
Yu Hsu <yhsu@kbbh.org>
2022-08-26 10:30:57 -0700

    Only display scripts when recipeint is payer.

--- ROI.js
+++ ROI.js
@@ -821,6 +821,8 @@ function setMedicaid(){
 }
 
 function whenCHAOMAP(){
+	$('tr').has('div[insuranceType=medicaid]').find('input').attr('title', '');
+
 	if($('#recipientSenderType').closest('table').parent().find('select').val() == $('#recipientSenderType').closest('table').parent().find('option').filter(function (){ return $(this).html() == 'Payer'}).val()){
 		if($('tr').has('div[id=payerName]').find('input').val() == 'CHA/OMAP'){
 			$('tr').has('div[insuranceType=medicaid]').find('input').attr('title', '<ul><li>As a Medicaid member, your insurance requires you release all KBBH records to all local Medicaid payers in order to receive services.  This includes, but is not limited to mental health and substance use disorder records, regardless of past or future services in these areas.</li></ul>');
@@ -834,9 +836,6 @@ function whenCHAOMAP(){
 		}
 		else if($('tr').has('div[id=payerName]').find('input').val() != null && $('tr').has('div[id=payerName]').find('input').val() != 'CHA/OMAP' && $('tr').has('div[id=payerName]').find('input').val() != 'Medicare'){
 			$('tr').has('div[insuranceType=medicaid]').find('input').attr('title', '<ul><li>Your insurance requires you release records related to both mental health and substance use disorders in order for KBBH to bill your insurance, regardless of past or future services in these areas.  Without this release, we will not be able to bill your insurance and you will be financially responsible for any charges to your account.</li></ul>');
-		}
-		else{
-			$('tr').has('div[insuranceType=medicaid]').find('input').attr('title', '');
 		}
 	}
 }
