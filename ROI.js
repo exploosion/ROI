932f28ba1ecbc8a2c1bc77a823397d745493c2b1
Yu Hsu <yhsu@kbbh.org>
2026-01-22 13:09:40 -0800

    Adding BCBS eradication and ROI Valid Logic

--- ROI.js
+++ ROI.js
@@ -911,3 +911,108 @@ document.addEventListener('DOMContentLoaded', () => {
 function makeDropDownReadOnly(){
     $('tr').has('div[class*=passenger]').find('select').attr('style', '-webkit-appearance: none; text-indent: 1px; pointer-events: none;');
 }
+
+//Hide extra BCBS Options
+document.addEventListener("DOMContentLoaded", (event) => {
+  [...document.querySelector('#payerSelect').closest('table').querySelectorAll('option')].filter((option) => {
+    return option.title.includes('BCBS') || option.title.includes('Blue Cross/Blue Shield') || option.title.includes('Blue Cross') || option.title.includes('Blue Shield');
+  }).forEach((result) => {
+    if(result.title !== 'Blue Cross/Blue Shield'){
+      console.log(result.title);
+      result.style.display = 'none';
+    }
+  });
+});
+
+document.addEventListener("DOMContentLoaded", (event) => {
+  [...document.querySelector('#providerSelect').closest('table').querySelectorAll('option')].filter((option) => {
+    return option.title.includes('Sky Lakes');
+  }).forEach((result) => {
+    if(result.title !== 'Sky Lakes Medical Center And Affiliates'){
+      console.log(result.title);
+      result.style.display = 'none';
+    }
+  });
+});
+
+//ROI Valid Options
+function restrictValidDates(){
+  let recipientType = document.querySelector('#recipientSenderType').closest('table').querySelector('select');
+  let payerValue = [...document.querySelector('#recipientSenderType').closest('table').querySelectorAll('option')].filter((option) => {
+    return option.innerText === 'Payer';
+  })[0].value;
+  let providerValue = [...document.querySelector('#recipientSenderType').closest('table').querySelectorAll('option')].filter((option) => {
+    return option.innerText === 'Provider';
+  })[0].value;
+    let interpreterValue = [...document.querySelector('#recipientSenderType').closest('table').querySelectorAll('option')].filter((option) => {
+    return option.innerText === 'Interpreter';
+  })[0].value;
+  let otherValue = [...document.querySelector('#recipientSenderType').closest('table').querySelectorAll('option')].filter((option) => {
+    return option.innerText === 'Other (details below)';
+  })[0].value;
+  let validByLaw = document.querySelector('#validByLaw').closest('tr');  
+  let validByLawInput = document.querySelector('#validByLaw').closest('tr').querySelector('input');
+  let validByDate = document.querySelector('#validByDate').closest('tr');
+  let validByDateInput = document.querySelector('#validByDate').closest('tr').querySelector('input');
+  
+  if(recipientType.value === payerValue || recipientType.value === providerValue || recipientType.value === interpreterValue){
+    validByLawInput.style.pointerEvents = 'none';
+    validByDateInput.style.pointerEvents = 'auto';
+    validByLaw.hidden = false;
+    validByDate.hidden = true;
+    if(!validByLaw.querySelector('input').checked){
+      validByLaw.querySelector('input').click();
+    }
+  }else if(recipientType.value === otherValue){
+    validByLawInput.style.pointerEvents = 'auto';
+    validByDateInput.style.pointerEvents = 'none';
+    validByLaw.hidden = true; 
+    validByDate.hidden = false;
+    if(!validByDate.querySelector('input').checked){
+      validByDate.querySelector('input').click();
+    }
+  }else{
+    validByLawInput.style.pointerEvents = 'auto';
+    validByDateInput.style.pointerEvents = 'auto';
+    validByLaw.hidden = false; 
+    validByDate.hidden = false;
+  }
+  fillValidDates();
+}
+  
+//ROI Valid Update
+function fillValidDates(){
+  const oneYearInDays = 365;
+  let date = new Date();
+  let laterDate = new Date(date);
+  laterDate.setDate(date.getDate() + oneYearInDays);
+  let today = (date.getMonth() + 1) + '/' + date.getDate().toString().padStart(2, 0) + '/' + date.getFullYear().toString();
+  let yearLater =  (laterDate.getMonth() + 1) + '/' + laterDate.getDate().toString().padStart(2, 0) + '/' + laterDate.getFullYear().toString();
+  
+  let startDate = document.querySelector('#validDateStart').closest('table').querySelector('input');
+  let endDate = document.querySelector('#validDateEnd').closest('table').querySelector('input');
+  
+  if(document.querySelector('#validByDate').closest('tr').querySelector('input').checked){
+    if(startDate.value === ''){
+      startDate.value = today;
+    }
+    if(endDate.value === ''){
+      endDate.value = yearLater;
+    }
+  }else if(!document.querySelector('#validByDate').closest('tr').querySelector('input').checked){
+    startDate.value = '';
+    endDate.value = '';
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  fillValidDates();
+  [...document.querySelectorAll('.validOptions')].forEach((validOption) => {
+    validOption.addEventListener('change', fillValidDates);
+    validOption.addEventListener('mouseleave', fillValidDates);
+  });
+  
+  restrictValidDates();
+  document.querySelector('#recipientSenderType').closest('table').querySelector('select').addEventListener('change', restrictValidDates);
+  document.querySelector('#recipientSenderType').closest('table').querySelector('select').addEventListener('mouseleave', restrictValidDates);
+});
